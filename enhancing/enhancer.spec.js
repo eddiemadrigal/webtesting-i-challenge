8232852dc95f7d21d0c310b49e50c801c2229304
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-22 21:11:06 -0700

    testing file refactored

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,13 @@
 const { repair, succeed } = require('./enhancer.js');
 
 describe('enhancer.js', () => {
+  
   it('should run tests without errors', () => {
     expect(true).toBe(true);
   })
+
+  it('should return an object', () => {
+    expect(repair({"durability": "1"})).toEqual({ "durability": "1" });
+  })
+
 })
